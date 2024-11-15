387bc30d7c292c49e0af2cae1f1b87adb5738c50
kalwanichandni24 <kalwanichandni24@gmail.com>
2024-11-16 00:19:36 +0530

    files updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}', // Adjust the paths to your project structure
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',  // Include all folders where you use Tailwind
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customDarkBlue: '#001F3F', 
+        customBeige: "#EAE4DD",
+      },
+    },
   },
   plugins: [],
-}
+};
